bdb0b6637e88ee12baee4f5be6f3a0bdb4b30e4a
LuPunk <kovacs.luca.agota@gmail.com>
2017-01-03 17:29:50 +0100

    Download Manager v0.2
    [v0.2] 
    - Placing directories
    - "Compressed" folder added

--- down_m.py
+++ down_m.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 path = "C:\\Users\\Luca\\Downloads"
 dot = 0
@@ -8,9 +9,10 @@ files = os.listdir(path)
 def mover(file):
     torrent = [".torrent"]
     pic = [".jpg", ".jpeg", ".gif", ".png"]
-    music = [".mp3", ".flac", ".wav", ".ogg"]
+    music = [".mp3", ".flac", ".wav", ".ogg", ".midi"]
     pdf = ".pdf"
     exe = [".exe", ".msi"]
+    comp = [".zip", ".rar", ".7z"]
 
     dot = file.rfind(".")
 
@@ -24,6 +26,8 @@ def mover(file):
         os.rename(path + "\\" + file, path + "\PDFs\\" + file)
     elif file[dot:] in exe:
         os.rename(path + "\\" + file, path + "\Executables\\" + file)
+    elif file[dot:] in comp:
+        os.rename(path + "\\" + file, path + "\Compressed\\" + file)
     else:
         path_ok = False
         while path_ok == False:
@@ -35,6 +39,22 @@ def mover(file):
 
     return None
 
+def dir_mover(directory):
+    path_ok = False
+    while path_ok == False:
+        print("Where should I place this folder?: {}".format(directory))
+        alt_path = input("Folder: ")
+        if os.path.exists(path + "\\{}".format(alt_path)):
+            shutil.move(path + "\\{}".format(directory), path + "\\{0}\\{1}".format(alt_path, directory))
+            path_ok = True
+
+    return None
+
+ignorelist = ["Executables", "Images", "Music", "Torrents", "PDFs", "Compressed"]
 for check in files:
     if "." in check and check[0] != ".":
         mover(check)
+    else:
+        if check not in ignorelist:
+            dir_mover(check)
+
